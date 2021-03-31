63b6849840d10f13ae29d47eb4c3f43531d1af73
juuheek <jkim844@my.bcit.ca>
2021-03-30 22:18:54 -0700

    Juhee_merge change

--- ourapp/pages/index.js
+++ ourapp/pages/index.js
@@ -1,21 +1,14 @@
 
-import Signup from '../comps/CustomComponent3'
+import Component3 from '../comps/CustomComponent3'
+import Component4 from '../comps/custom-component4/'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
     <div>
-      <Signup />
-
-import Component4 from '../comps/custom-component4/'
-import styles from '../styles/Home.module.css'
-
-
-export default function Home(){
-  return (
-    <div>
+      <Component3 />
      <Component4/>
-
     </div>
+
   )
 } 
