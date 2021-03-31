06129d077f094afefed0c19ed4a375c68f6ed206
GlianBCIT <gpalma1@my.bcit.ca>
2021-03-30 22:55:42 -0700

    Glian_Changed Componet 4 name

--- ourapp/pages/index.js
+++ ourapp/pages/index.js
@@ -3,7 +3,7 @@
 import Component1 from '../comps/CustomComponent1';
 import Component2 from '../comps/CustomComponent2';
 import Component3 from '../comps/CustomComponent3'
-import Component4 from '../comps/custom-component4/'
+import Component4 from '../comps/Custom-Component4'
 
 
 export default function Home() {
