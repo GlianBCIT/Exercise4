90d0576d07841aad369ba516ec9e0e624be3a7ab
GlianBCIT <gpalma1@my.bcit.ca>
2021-03-30 21:48:59 -0700

    Glian_created component 2

---
+++ ourapp/comps/CustomComponent2/index.js
@@ -0,0 +1,17 @@
+function Component2UI(){
+return <div id="main">
+    <h3><span className="size">TY</span></h3>
+    <div className="column-end">
+    <p> Home </p>
+    <p> About</p>
+    <p> Shop</p>
+    <p> Contact</p>
+    <p> Services</p>
+    <p> Help</p>
+    </div>
+    <button>Get Started</button>
+
+</div>
+}
+
+export default Component2UI;
