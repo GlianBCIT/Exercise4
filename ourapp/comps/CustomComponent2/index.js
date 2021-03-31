96d5df4bc60ca6d1b0d3d8547b8b482b4f6065c8
GlianBCIT <gpalma1@my.bcit.ca>
2021-03-30 22:28:20 -0700

    Glian_Changed components to inline style

--- ourapp/comps/CustomComponent2/index.js
+++ ourapp/comps/CustomComponent2/index.js
@@ -1,15 +1,63 @@
 function Component2UI(){
-return <div id="main">
-    <h3><span className="size">TY</span></h3>
-    <div className="column-end">
-    <p> Home </p>
-    <p> About</p>
-    <p> Shop</p>
-    <p> Contact</p>
-    <p> Services</p>
-    <p> Help</p>
+return <div style={{
+    fontFamily: "Arial, Helvetica, sans-serif",
+    margin: 0,
+    display: 'flex',
+    flexDirection: 'row',
+    backgroundColor: '#26325B',
+    width: 900,
+    height: 100,
+}} >
+    <h3 style={{
+            color: 'white',
+            margin: '0 0 0 20',
+    }}>
+        <span style={{
+        fontSize: 80,
+    }}>
+        TY</span></h3>
+
+    <div style={{
+            display: 'flex',
+            flexDirection: 'row',
+    }}>
+
+    <p style={{
+        color: 'white',
+        margin: '35 35 10 35',
+    }}> Home </p>
+
+    <p style={{
+        color: 'white',
+        margin: '35 35 10 35',
+    }}> About</p>
+
+    <p style={{
+        color: 'white',
+        margin: '35 35 10 35',
+    }}> Shop</p>
+
+    <p style={{
+        color: 'white',
+        margin: '35 35 10 35',
+    }}> Contact</p>
+
+    <p style={{
+        color: 'white',
+        margin: '35 35 10 35',
+    }}> Services</p>
+
+    <p style={{
+        color: 'white',
+        margin: '35 35 10 35',
+    }}> Help</p>
+
     </div>
-    <button>Get Started</button>
+
+    <button style={{
+        width: 100,
+        backgroundColor: 'white',
+    }}>Get Started</button>
 
 </div>
 }
--- ourapp/pages/index.js
+++ ourapp/pages/index.js
@@ -1,23 +1,14 @@
 
-import styles from '../styles/main.module.css';
 import Component2 from '../comps/CustomComponent2';
 
 
-// this is our workflow.html
 function Home() {
 
-  // we do not need a workglow.html and do the += innerHTML stuff
-  // can't use class, have to be className
+
   return (
-    <div className="main">
+    <div>
         <Component2 />
     </div>
-    // <div className={styles.main}>
-    //   Home Page
-    //   <Avatar url="https://placekitten.com./200/200"/>
-    //   <Avatar url="https://placekitten.com./300/300"/>
-    //   <Avatar />
-    // </div>
     )
   }
 
